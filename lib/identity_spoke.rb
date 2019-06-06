34980a186f865dd0351a88374f58e4bde53ba622
Joni Gear <joni@getup.org.au>
2019-06-06 10:25:59 +1000

    Fix error on sync audit - send message_id

--- lib/identity_spoke.rb
+++ lib/identity_spoke.rb
@@ -195,7 +195,7 @@ module IdentitySpoke
         contact_response.save! if contact_response.new_record?
       end
     end
-    Sync.update_report_if_last_record_for_import(sync_id, call_id)
+    Sync.update_report_if_last_record_for_import(sync_id, message_id)
   end
 
   def self.fetch_new_opt_outs(sync_id, force: false)
