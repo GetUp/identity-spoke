05723928b274ba089ed1b44eab01b21671cca4b9
Andy South <andysouth@getup.org.au>
2022-12-12 10:08:14 +1100

    Ensure that connection pool size is defaulting correctly from RAILS_MAX_THREADS

--- app/lib/identity_spoke/application_record.rb
+++ app/lib/identity_spoke/application_record.rb
@@ -5,7 +5,7 @@ module IdentitySpoke
     def self.set_db_pool_size(db_url_string)
       if db_url_string
         db_url = URI.parse db_url_string
-        pool_size = ENV.fetch('RAILS_MAX_THREADS') || 5
+        pool_size = ENV['RAILS_MAX_THREADS'] || 5
         qs_hash = db_url.query ? CGI.parse(db_url.query) : { }
         qs_hash['pool'] = pool_size if !qs_hash['pool']
         db_url.query = URI.encode_www_form(qs_hash)
