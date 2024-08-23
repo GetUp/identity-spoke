3efff00c7d3c28868cd06a99da9c6ce562d3ed20
Michael Gratton <michaelg@getup.org.au>
2024-08-23 13:15:42 +1000

    Update application and env config files from current upstream

--- spec/test_identity_app/config/application.rb
+++ spec/test_identity_app/config/application.rb
@@ -1,25 +1,56 @@
+require 'dotenv'
+Dotenv.load
+
 require_relative 'boot'
+require 'rails/all'
 
-# Pick the frameworks you want:
-require "active_record/railtie"
-require "action_controller/railtie"
-require "action_view/railtie"
-require "action_mailer/railtie"
-require "active_job/railtie"
-require "action_cable/engine"
-# require "rails/test_unit/railtie"
-# require "sprockets/railtie"
-
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-require "identity_spoke"
 
-module TestIdentityApp
+module Identity
+  VERSION = '22.06'.freeze
+
   class Application < Rails::Application
+    class ExceptionHandlerMiddleware
+      def initialize(app)
+        @app = app
+      end
+
+      def call(env)
+        @app.call(env)
+      rescue ActionController::BadRequest => e
+        # handle malformed requests -- we don't want newrelic/airbrake/... flooded
+        # with errors caused by client bugs
+        return [400, { "Content-Type" => "text/plain" }, [e.to_s]]
+      end
+    end
+
+    private_constant :ExceptionHandlerMiddleware
+
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.1
+    config.load_defaults 6.0
+
+    # Disabled protected attributes for now, this allows us to continue
+    # using weak params, until we refactor and move to strong params
+    config.action_controller.permit_all_parameters = true
 
     # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
+    config.generators do |g|
+      g.assets false
+      g.helper false
+    end
+
+    # Ensure locale doesn't leak between requests in multi-threaded envrionments.
+    # See: https://github.com/svenfuchs/i18n/pull/382
+    config.middleware.use ::I18n::Middleware
+
+    # Load app settings
+    require_relative '../app/lib/settings'
+
+    config.middleware.use ExceptionHandlerMiddleware
   end
 end
--- spec/test_identity_app/config/environments/development.rb
+++ spec/test_identity_app/config/environments/development.rb
@@ -12,19 +12,27 @@ Rails.application.configure do
   # Show full error reports.
   config.consider_all_requests_local = true
 
+  # allow identity-web to be used as hostname
+  config.hosts << 'identity-web'
+
   # Enable/disable caching. By default caching is disabled.
+  # Run rails dev:cache to toggle caching.
   if Rails.root.join('tmp/caching-dev.txt').exist?
     config.action_controller.perform_caching = true
+    config.action_controller.enable_fragment_cache_logging = true
 
     config.cache_store = :memory_store
     config.public_file_server.headers = {
-      'Cache-Control' => "public, max-age=#{2.days.seconds.to_i}"
+      'Cache-Control' => "public, max-age=#{2.days.to_i}"
     }
   else
     config.action_controller.perform_caching = false
 
     config.cache_store = :null_store
   end
+
+  # Store uploaded files on the local file system (see config/storage.yml for options).
+  config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
@@ -37,6 +45,12 @@ Rails.application.configure do
   # Raise an error on page load if there are pending migrations.
   config.active_record.migration_error = :page_load
 
+  # Opt out of legacy connection handling (raise errors)
+  config.active_record.legacy_connection_handling = false
+
+  # Highlight code that triggered database queries in logs.
+  config.active_record.verbose_query_logs = true
+
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
@@ -45,10 +59,31 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
-  # Raises error for missing translations
+  # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
+
+  # Use default logging formatter so that PID and timestamp are not suppressed.
+  config.log_formatter = ::Logger::Formatter.new
+
+  config.log_level = Settings.log_level.downcase.to_sym
+
+  if ENV["RAILS_LOG_TO_STDOUT"].present?
+    logger = ActiveSupport::Logger.new(STDOUT)
+    logger.formatter = config.log_formatter
+    logger.level = Logger.const_get(Settings.log_level.upcase)
+    config.logger = ActiveSupport::TaggedLogging.new(logger)
+  end
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
-  # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  if ENV["HOME"].include?('vagrant') || ENV["M1_MAC_DOCKER"].present?
+    config.file_watcher = ActiveSupport::FileUpdateChecker
+  else
+    config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  end
+
+  config.after_initialize do
+    Bullet.enable = true
+    Bullet.bullet_logger = true
+  end
 end
